c4e2200bb787ca2bcd94e92fa6fdd913b15b2e53
NeoMemb <arioriabdulrafiu220@gmail.com>
2025-06-01 06:51:44 +0100

    Changed the function displayTasks

--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
 const inputBar = document.querySelector("input.input-bar");
 const addBtn = document.querySelector("button.add-task");
 const taskList = document.querySelector("ol.task-list");
+const doneList = document.querySelector("ul.done-tasks");
 let checkBtn, taskText;
 
 //  When the user types a task and clicks "Add" =>
@@ -13,7 +14,8 @@ let checkBtn, taskText;
 //                        obj || array
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 let done = JSON.parse(localStorage.getItem("done")) || [];
-displayTasks();
+addBorder(taskList);
+displayTasks(taskList, tasks);
 
 // 2. Add task
 addBtn.addEventListener("click", () => {
@@ -22,7 +24,7 @@ addBtn.addEventListener("click", () => {
     tasks.push(taskText);
     inputBar.value = ""; // Clear input
     saveTasks("tasks", tasks);
-    displayTasks();
+    displayTasks(taskList, tasks);
   }
 });
 
@@ -35,32 +37,28 @@ inputBar.addEventListener("keydown", (e) => {
       tasks.push(taskText);
       inputBar.value = ""; // Clear input
       saveTasks("tasks", tasks);
-      displayTasks();
+      displayTasks(taskList, tasks);
     }
     }
 });
 
-checkBtn.addEventListener("change", (e) =>{
-  if (e.target.checked){
-    // saveTasks("done", e.parentElement.innerHTML)
-    console.log(e.parentElement);
-  }
-});
-
-function saveTasks(a, b) {
-  // saveTasks: save a json file stringify
-// @a: the key
-// @b: the value to bestringify
-  localStorage.setItem(a, JSON.stringify(b));
-}
+//  ******** FUNCTIONS **********
 
 // 4. Display tasks
-function displayTasks() {
+//Changes to be done(1 & 2 done!!!):
+  //  a. Change every "taskList" --> "to"
+  //  b. Then "tasks" --> "from"
+  //  c. Update everywhere the function is called
+  
+// function displayTasks(to, from) {
+function displayTasks(to, from) {
   // Clear existing items in the <ol>...</ol>
-  taskList.innerHTML = "";
+
+  
+  to.innerHTML = "";
 
   // Then iterate in each items of obj || array
-  tasks.forEach((taskItem, index) => {
+  from.forEach((taskItem, index) => {
 
     // ALL THESE WILL BE WHAT WILL BE DISPLAYED, WHEN ADD TASK BTN IS CLICKED
 
@@ -70,27 +68,55 @@ function displayTasks() {
 
     // Create a button for del
     const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
+    deleteBtn.textContent = " Delete ";
     deleteBtn.className = "del-btn";
     deleteBtn.onclick = () => {
-      tasks.splice(index, 1);
-      saveTasks("tasks", tasks);
-      displayTasks();
+      if (confirm("Do you want to delete this?"))
+      {
+        from.splice(index, 1);
+        saveTasks("tasks", tasks);
+        addBorder(to);
+        displayTasks(to, from);
+      }
     };
 
+    // check-btn for each li
     checkBtn = document.createElement("input");
-
     const attributes = {
       type: "checkbox",
       class: "check-box"
     };
-
     for (let key in attributes){
       checkBtn.setAttribute(key, attributes[key]);
     }
 
     li.appendChild(deleteBtn);
     li.appendChild(checkBtn);
-    taskList.appendChild(li);
+    to.appendChild(li);
   });
+  addBorder(to);
+}
+if (checkBtn.checked){
+  done.push(target.parentElement);
+  saveTasks(done, "done");
+  checkBtn.parentElement.remove();
+  displayTasks(taskList, tasks);
+  displayTasks(doneList, done);
+}
+displayTasks(doneList, done);
+
+function addBorder(l){
+  if (l.children.length !== 0)
+  {
+    //  I want to do something more here
+    l.style.border = "2px solid grey";
+    l.style.borderRadius = ".5em";
+  }
+  taskList.parentElement.style.border = "none";
+}
+function saveTasks(a, b) {
+// saveTasks: save a json file stringify
+// @a: the key
+// @b: the value to bestringify
+  localStorage.setItem(a, JSON.stringify(b));
 }
